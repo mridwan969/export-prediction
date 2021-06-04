680b82c118249faea36f7717dea218f27c66101a
mridwan969 <85182532+mridwan969@users.noreply.github.com>
2021-06-04 22:53:47 +0700

    add database file

---
+++ db_expert.sql
@@ -0,0 +1,59 @@
+/*
+SQLyog Community v13.1.6 (64 bit)
+MySQL - 10.4.16-MariaDB : Database - db_expert
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`db_expert` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+
+USE `db_expert`;
+
+/*Table structure for table `commodities` */
+
+DROP TABLE IF EXISTS `commodities`;
+
+CREATE TABLE `commodities` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+/*Data for the table `commodities` */
+
+insert  into `commodities`(`id`,`name`,`created_at`,`updated_at`) values 
+(1,'Apples, fresh',NULL,NULL),
+(2,'Coffee, not roasted, not decaffeinated',NULL,NULL),
+(3,'Potatoes, fresh or chilled except seed',NULL,NULL),
+(4,'Tomatoes, fresh or chilled',NULL,NULL),
+(5,'Shrimps and prawns, prepared or preserved',NULL,NULL);
+
+/*Table structure for table `migrations` */
+
+DROP TABLE IF EXISTS `migrations`;
+
+CREATE TABLE `migrations` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+/*Data for the table `migrations` */
+
+insert  into `migrations`(`id`,`migration`,`batch`) values 
+(1,'2021_05_29_162419_create_commodities_table',1);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
